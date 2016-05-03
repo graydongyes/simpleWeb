7aa893af95ac119acdef75720b9eb4fe0c4be759
袁渊 <yuan.yuan1@baozun.com>
2016-05-04 00:01:54 +0800

    注释alert

--- src/main/webapp/comm/skin/js/main.js
+++ src/main/webapp/comm/skin/js/main.js
@@ -1,14 +1,13 @@
 function iFrameHeight() {
-	var ifm = document.getElementById('main_page');
-	
-	var left_ifm = document.getElementById('left_window');
-	var winWidth = document.documentElement.clientWidth;
-	var leftWidth = document.getElementById('left_window').scrollWidth;
-	   ifm.width = winWidth - leftWidth - 5;
-	   
-   var winHeight = document.documentElement.clientHeight;
-   var topHeight = document.getElementById('top_window').scrollHeight;
-   		ifm.height = winHeight - topHeight - 5;
+ 	var ifm = document.getElementById('main_page');
+ 	
+ 	var left_height = document.getElementById('left_window');
+ 	var winWidth = document.documentElement.clientWidth;
+ 	var leftWidth = document.getElementById('left_window').scrollWidth;
+ 	   ifm.width = winWidth - leftWidth - 5;
+// 	   alert(left_height.scrollHeight);
+    var winHeight = document.documentElement.clientHeight;
+    var topHeight = document.getElementById('top_window').scrollHeight;
+    		ifm.height = winHeight - topHeight - 5;
    		
-//	left_ifm.height=winHeight - topHeight - 5;
 }
